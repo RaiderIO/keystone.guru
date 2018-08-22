cbaf6d35274dfdb5595ae536a5d649f037e61423
Wouter <wouter.koppenol@gmail.com>
2018-08-22 22:36:04 +0200

    AdSense by Google. Yeah I know we all hate ads, but I'm trying to feed myself. I'll forgive you if you block them.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,6 +52,13 @@
             // $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
         }
     </script>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-2985471802502246",
+        enable_page_level_ads: true
+      });
+    </script>
 </head>
 <body>
 <div id="app">
