5daf885dad3d45f80fdeca87102b443573a30c2b
Wouter <wouter.koppenol@gmail.com>
2018-08-25 00:06:19 +0200

    Removed the old NPC seeders, this is now completely done by exporting the file and re-importing it (between instances). Removed all bugs from the new DungeonDataSeeder, that works properly now including most edge cases. There's only one hard coded thing which I want to look at later, but that'd involve changing the layout of the exported files to be completely de-normalized.

--- database/seeds/DungeonDataSeeder.php
+++ database/seeds/DungeonDataSeeder.php
@@ -34,41 +34,37 @@ class DungeonDataSeeder extends Seeder
 
         // For each expansion
         foreach ($rootDirIterator as $expansionShortnameDir) {
-            $this->command->info('Expansion: ' . basename($expansionShortnameDir));
+            $this->command->info('Expansion ' . basename($expansionShortnameDir));
             $expansionDirIterator = new FilesystemIterator($expansionShortnameDir);
             // For each dungeon inside an expansion dir
             foreach ($expansionDirIterator as $dungeonKeyDir) {
-                // Temp to reduce spam
-                if (!str_contains($dungeonKeyDir, "hallsofvalor")) {
-                    continue;
-                }
-                $this->command->info('Importing dungeon: ' . basename($dungeonKeyDir));
+                $this->command->info('- Importing dungeon ' . basename($dungeonKeyDir));
 
                 // Import NPCs
-                $this->command->info('Importing NPCs...');
+                $this->command->info('-- Importing npcs');
                 // Get contents
-                $count = $this->_loadModelsFromFile($dungeonKeyDir . '/npcs.json', '\App\Models\Npc');
-                $this->command->info(sprintf('Imported %s NPCs', $count));
-
+                $npcFileName = $dungeonKeyDir . '/npcs.json';
+                $count = $this->_loadModelsFromFile($npcFileName, '\App\Models\Npc');
+                $this->command->info(sprintf('-- Imported %s (%s into %s)', str_replace($rootDir, '', $npcFileName), $count, 'npcs'));
 
                 $floorDirIterator = new FilesystemIterator($dungeonKeyDir);
                 // For each floor inside a dungeon dir
                 foreach ($floorDirIterator as $floorDir) {
                     // Skip loose files (npcs.json looking at you)
                     if (is_dir($floorDir)) {
-                        $this->command->info('Importing floor ' . basename($floorDir));
+                        $this->command->info('-- Importing floor ' . basename($floorDir));
 
                         $importFileIterator = new FilesystemIterator($floorDir);
                         // For each file inside a floor
                         foreach ($importFileIterator as $importFile) {
                             $tableName = basename($importFile, '.json');
-                            $this->command->info('Importing ' . $tableName);
+                            $this->command->info('--- Importing ' . $tableName);
 
                             if (!array_key_exists($tableName, $nameMapping)) {
-                                $this->command->error('Unable to find table->model mapping for file ' . $importFile);
+                                $this->command->error('--- Unable to find table->model mapping for file ' . $importFile);
                             } else {
                                 $count = $this->_loadModelsFromFile($importFile, $nameMapping[$tableName]);
-                                $this->command->info(sprintf('Imported %s %s', $count, $tableName));
+                                $this->command->info(sprintf('--- Imported %s (%s into %s)', str_replace($rootDir, '', $importFile), $count, $tableName));
                             }
 
                         }
@@ -88,30 +84,60 @@ class DungeonDataSeeder extends Seeder
      */
     private function _loadModelsFromFile($filePath, $modelClassName)
     {
-        $this->command->info('>> _loadModelsFromFile ' . $filePath . ' ' . $modelClassName);
+        // $this->command->info('>> _loadModelsFromFile ' . $filePath . ' ' . $modelClassName);
         // Load contents
         $modelJson = file_get_contents($filePath);
         // Convert to models
         $modelsData = json_decode($modelJson, true);
 
+        // Pre-fetch all valid columns to make the below loop a bit faster
+        $modelColumns = Schema::getColumnListing($modelClassName::newModelInstance()->getTable());
+
         // Do some php fuckery to make this a bit cleaner
         foreach ($modelsData as $modelData) {
-            foreach($modelData as $key => $value){
-                // Encountered an object; we gotta de-normalize it
-                if(is_array($value)){
-                    $this->command->info(json_encode($modelData));
-                    /** @var $value \Illuminate\Database\Eloquent\Model */
-                    $modelData[$key . '_id'] = $value['id'];
 
-                    // Remove the object; we can now save the thing in the DB properly
+            for ($i = 0; $i < count($modelData); $i++) {
+                $keys = array_keys($modelData);
+                $key = $keys[$i];
+                $value = $modelData[$key];
+
+                // $this->command->info(json_encode($key) . ' ' . json_encode($value));
+
+
+                // foreach ($modelData as $key => $value) {
+                // Encountered an object; we gotta de-normalize it
+                if (is_array($value)) {
+                    // If it's a nested model
+                    if (isset($value['id'])) {
+                        $modelData[$key . '_id'] = $value['id'];
+                    } // @TODO fix this hack?
+                    else if ($key === 'vertices') {
+                        foreach ($value as $vertex) {
+                            /** @var \Illuminate\Database\Eloquent\Model $vertexModelClassName */
+                            $vertexModelClassName = ($modelClassName . 'Vertex');
+                            // Rebuild the reference to the original model (EnemyPack -> enemy_pack)
+                            $snakedClassName = snake_case(last(explode('\\', $modelClassName)));
+                            $vertex[$snakedClassName . '_id'] = $modelData['id'];
+
+                            // $vertex->
+                            $vertexModelClassName::create($vertex);
+                        }
+                    }
+                }
+
+                // The column may not be set due to objects appearing in this array that need to be de-normalized
+                if (!in_array($key, $modelColumns)) {
                     unset($modelData[$key]);
+                    $i--;
                 }
             }
+
+            // $this->command->info("Creating model " . json_encode($modelData));
             // Create and save a new instance to the database
             $modelClassName::create($modelData);
         }
 
-        $this->command->info('OK _loadModelsFromFile ' . $filePath . ' ' . $modelClassName);
+        // $this->command->info('OK _loadModelsFromFile ' . $filePath . ' ' . $modelClassName);
         return count($modelsData);
     }
 
