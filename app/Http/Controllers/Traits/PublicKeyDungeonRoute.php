1e1a3047b2880c47ee0f00aae44732b404ea98cc
Wouter <wouter.koppenol@gmail.com>
2018-09-07 01:04:59 +0200

    Hotfix for unauthenticated users not being able to see routes and killzones.

--- app/Http/Controllers/Traits/PublicKeyDungeonRoute.php
+++ app/Http/Controllers/Traits/PublicKeyDungeonRoute.php
@@ -6,13 +6,15 @@ use App\Models\DungeonRoute;
 use Illuminate\Support\Facades\Auth;
 use Mockery\Exception;
 
-trait PublicKeyDungeonRoute {
+trait PublicKeyDungeonRoute
+{
     function _getDungeonRouteFromPublicKey($publicKey)
     {
         $dungeonRoute = DungeonRoute::where('public_key', '=', $publicKey)->firstOrFail();
 
         // @TODO handle this in a policy?
-        if ($dungeonRoute->author_id !== Auth::user()->id) {
+        $user = Auth::user();
+        if ($user !== null && $dungeonRoute->author_id !== $user->id) {
             throw new Exception('Unauthorized');
         }
 
