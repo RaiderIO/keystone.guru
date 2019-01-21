cb9460541821eb35db6915ddaf717db4970ae50b
Wotuu <support@keystone.guru>
2019-01-21 15:46:50 +0100

    #102 Some updates to the Vue code, but I'm aborting this here. Vue is too different to convert something, I'd also end up with fragmented translations which I don't want. It's going to be a bit of a mess, for my Vue.js learnings I'll create a different project. Perhaps I can make something new with Vue.js for Keystone.guru but converting something is not the way.

--- resources/assets/js/components/Profile.vue
+++ resources/assets/js/components/Profile.vue
@@ -1,19 +1,76 @@
+<i18n>
+    {
+    "en": {
+    "Profile": "Profile",
+    "Patreon": "Patreon",
+    "Change password": "Change password",
+    "Privacy": "Privacy",
+    "Email": "Email",
+    "Select your region": "Select your region",
+    "Select region": "Select region",
+    "Submit": "Submit",
+    "Unlink from Patreon": "Unlink from Patreon",
+    "Your account is linked to Patreon. Thank you!": "Your account is linked to Patreon. Thank you!",
+    "Link to Patreon": "Link to Patreon",
+    "In order to claim your Patreon rewards, you need to link your Patreon account": "In order to claim your Patreon
+    rewards, you need to link your Patreon account",
+    "Patreon implementation is experimental. If your rewards are not available after linking with your Patreon, please
+    contact me directly on Discord or Patreon and I will fix it for you.":
+    "Patreon implementation is experimental. If your rewards are not available after linking with your Patreon, please
+    contact me directly on Discord or Patreon and I will fix it for you.",
+    "Current password": "Current password",
+    "New password": "New password",
+    "New password (confirm)": "New password (confirm)",
+    "Google Analytics cookies opt-out": "Google Analytics cookies opt-out",
+    "Google Adsense no personalized ads": "Google Adsense no personalized ads"
+    },
+    }
+</i18n>
+
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+        <ul class="nav nav-tabs mb-3" id="myTab" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" id="profile-tab" data-toggle="tab" href="#profile" role="tab"
+                   aria-controls="profile" aria-selected="true">{{ $t('Profile') }}</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="patreon-tab" data-toggle="tab" href="#patreon" role="tab"
+                   aria-controls="patreon" aria-selected="false">{{ $t('Patreon') }}</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="change-password-tab" data-toggle="tab" href="#change-password" role="tab"
+                   aria-controls="change-password" aria-selected="false">{{ $t('Change password') }}</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="privacy-tab" data-toggle="tab" href="#privacy" role="tab"
+                   aria-controls="contact" aria-selected="false">{{ $t('Privacy') }}</a>
+            </li>
+        </ul>
 
-                    <div class="panel-body">
-                        I'm an example component! {{ test3 }}
-                    </div>
+        <div class="tab-content" id="myTabContent">
+
+            <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                <div class="form-group">
+                    <label for="email">{{ $t('Email') }}</label>
+                    <input type="text" name="email" id="email" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <label for="game_server_region_id">{{ $t('Select your region') }}</label>
+                    <select name="game_server_region_id" id="game_server_region_id" class="form-control">
+                        <option v-for="gameserver in gameservers" value="{{ gameserver.id }}">
+                            {{ gameserver.name }}
+                        </option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    timezone
                 </div>
             </div>
         </div>
     </div>
 </template>
-
+<!-- array_merge(['-1' => 'Select region'], \App\Models\GameServerRegion::all()->pluck('name', 'id')->toArray()) -->
 <script>
     export default {
         mounted() {
@@ -21,8 +78,16 @@
         },
         data() {
             return {
-                'username': 'test',
-                'test3': 'asdf'
+                username: 'test',
+                test3: 'asdf',
+                gameservers: [{
+                    id: -1,
+                    name: 'Select Server'
+                }, {
+                    id: 1,
+                    name: 'test'
+                }]
+
             }
         }
     }
