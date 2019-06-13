43adfa16e1b3c706d5b6ce0c0c7bad2aeec040eb
Wouter Koppenol <wouter.koppenol@gmail.com>
2019-06-13 23:55:47 +0200

    #137 Fixed a tooltip format.

--- resources/assets/js/dashboard/customcharts.js
+++ resources/assets/js/dashboard/customcharts.js
@@ -29,7 +29,7 @@ class LineChart {
                             time: {
                                 parser: this.timeFormat,
                                 // round: 'day'
-                                tooltipFormat: 'll HH:mm'
+                                tooltipFormat: 'll'
                             },
                             scaleLabel: {
                                 display: true,
