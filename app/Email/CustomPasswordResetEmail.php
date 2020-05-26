2dba86067404bbc33c9e1b37d80243c2d614f81e
Wouter Koppenol <wouter.koppenol@gmail.com>
2020-05-26 23:29:42 +0200

    #286 Updated to laravel 5.8

--- app/Email/CustomPasswordResetEmail.php
+++ app/Email/CustomPasswordResetEmail.php
@@ -15,7 +15,7 @@ class CustomPasswordResetEmail extends \Illuminate\Auth\Notifications\ResetPassw
     {
         return (new \Illuminate\Notifications\Messages\MailMessage())
             ->line('We are sending this email because we recieved a forgot password request.')
-            ->action('Reset Password', url(config('app.url') . route('password.reset', $this->token, false)))
+            ->action('Reset Password', url(config('app.url') . route('password.reset', ['token' => $this->token], false)))
             ->line('If you did not request a password reset, no further action is required. Please contact us if you did not submit this request.');
     }
 }
