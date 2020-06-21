734f861ca944f4422502dd5a5c969493c237476f
Wouter Koppenol <wouter.koppenol@gmail.com>
2020-06-22 00:27:08 +0200

    #355 Teeming and seasonal index are now cloned properly when cloning a route.

--- app/Models/DungeonRoute.php
+++ app/Models/DungeonRoute.php
@@ -636,6 +636,8 @@ class DungeonRoute extends Model
         $dungeonroute->dungeon_id = $this->dungeon_id;
         $dungeonroute->faction_id = $this->faction_id;
         $dungeonroute->title = sprintf('%s (%s)', $this->title, __('clone'));
+        $dungeonroute->teeming = $this->teeming;
+        $dungeonroute->seasonal_index = $this->seasonal_index;
         $dungeonroute->clone_of = $this->public_key;
         $dungeonroute->public_key = DungeonRoute::generateRandomPublicKey();
         $dungeonroute->published = $published;
