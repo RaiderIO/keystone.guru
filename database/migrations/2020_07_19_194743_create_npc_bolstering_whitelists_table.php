19454cb512ef74e902f0e41c1220d10186b2b9c9
Wouter Koppenol <wouter.koppenol@gmail.com>
2020-07-20 23:05:18 +0200

    #388 Added NpcBolsteringWhitelist model, renamed table to be plural form.

--- database/migrations/2020_07_19_194743_create_npc_bolstering_whitelists_table.php
+++ database/migrations/2020_07_19_194743_create_npc_bolstering_whitelists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNpcBolsteringWhitelistTable extends Migration
+class CreateNpcBolsteringWhitelistsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateNpcBolsteringWhitelistTable extends Migration
      */
     public function up()
     {
-        Schema::create('npc_bolstering_whitelist', function (Blueprint $table) {
+        Schema::create('npc_bolstering_whitelists', function (Blueprint $table) {
             $table->id();
             $table->integer('npc_id');
             $table->integer('whitelist_npc_id');
@@ -30,6 +30,6 @@ class CreateNpcBolsteringWhitelistTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('npc_bolstering_whitelist');
+        Schema::dropIfExists('npc_bolstering_whitelists');
     }
 }
