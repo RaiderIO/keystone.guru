3012c1640801cc0efe3e16a7f481e9fa7c866227
Wouter Koppenol <wouter.koppenol@gmail.com>
2020-09-25 21:58:20 +0200

    #464 Added default for unskippable column

--- database/migrations/2020_09_13_120903_add_unskippable_column_to_enemies_table.php
+++ database/migrations/2020_09_13_120903_add_unskippable_column_to_enemies_table.php
@@ -14,7 +14,7 @@ class AddUnskippableColumnToEnemiesTable extends Migration
     public function up()
     {
         Schema::table('enemies', function (Blueprint $table) {
-            $table->boolean('unskippable')->after('faction');
+            $table->boolean('unskippable')->default(false)->after('faction');
         });
     }
 
