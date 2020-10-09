5758bd7616301ce57c9a8b4a1096ff0e2af2667b
Wotuu <wouter.koppenol@gmail.com>
2020-10-09 12:11:57 +0200

    #479 Properly fixed the 500 error that plagued production.

--- resources/views/common/dungeon/grid.blade.php
+++ resources/views/common/dungeon/grid.blade.php
@@ -2,8 +2,8 @@
 /** @var $expansionService \App\Service\Expansion\ExpansionService */
 /** @var $expansion \App\Models\Expansion */
 $expansion = $expansionService->getCurrentExpansion();
-$rowCount = 4;
-$colCount = (int) round($expansion->dungeons()->active()->count() / $rowCount);
+$colCount = 3;
+$rowCount = (int)ceil($expansion->dungeons()->active()->count() / $colCount);
 
 $names = isset($names) ? $names : true;
 $links = isset($links) ? $links : collect();
@@ -13,25 +13,29 @@ $links = isset($links) ? $links : collect();
 <?php for( $i = 0; $i < $rowCount; $i++ ) { ?>
 <div class="row no-gutters mt-2">
     <?php for( $j = 0; $j < $colCount; $j++ ) {
-    $dungeon = $expansion->dungeons->get($i * $colCount + $j);
-    $link = $links->where('dungeon', $dungeon->key)->first();
-    ?>
-    <div class="col-md-4">
-        @if($names)
-            <div class="text-center font-weight-bold"> {{ __($dungeon->name) }} </div>
-        @endif
+    $index = $i * $colCount + $j;
+    if( $expansion->dungeons->has($index) ){
+        $dungeon = $expansion->dungeons->get($index);
+        $link = $links->where('dungeon', $dungeon->key)->first();
+        ?>
+        <div class="col-md-4">
+            @if($names)
+                <div class="text-center font-weight-bold"> {{ __($dungeon->name) }} </div>
+            @endif
 
-        @isset($link)
-            <a href="{{ __($link['link']) }}">
-                @endisset
+            @isset($link)
+                <a href="{{ __($link['link']) }}">
+                    @endisset
 
-                <img src="images/dungeons/{{ $dungeon->key }}.jpg" style="width: 100%" alt="{{ __($dungeon->name) }}"/>
+                    <img src="images/dungeons/{{ $dungeon->key }}.jpg" style="width: 100%" alt="{{ __($dungeon->name) }}"/>
 
-                @isset($link)
-            </a>
-        @endisset
-    </div>
-    <?php } ?>
+                    @isset($link)
+                </a>
+            @endisset
+        </div>
+        <?php
+        }
+    } ?>
 </div>
 <?php } ?>
 
