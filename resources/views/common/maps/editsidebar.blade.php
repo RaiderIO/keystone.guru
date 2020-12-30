3e31e426995d18c59828598e9e914a35c3c93289
Wouter Koppenol <wouter.koppenol@gmail.com>
2020-12-30 11:59:57 +0100

    #544 Added sharing section to view routes as well so that people can embed other people's routes. Removed some redundant information from the view page.

--- resources/views/common/maps/editsidebar.blade.php
+++ resources/views/common/maps/editsidebar.blade.php
@@ -33,39 +33,7 @@ if (\Illuminate\Support\Facades\Auth::check()) {
     'id' => 'editsidebar'
 ])
     @isset($show['sharing'])
-        <!-- Shareable link -->
-        <div class="form-group">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">{{ __('Share') }}</h5>
-                    <div class="row mb-2">
-                        <div class="input-group">
-                            {!! Form::text('map_shareable_link', route('dungeonroute.view', ['dungeonroute' => $model->public_key]),
-                            ['id' => 'map_shareable_link', 'class' => 'form-control', 'readonly' => 'readonly']) !!}
-                            <div class="input-group-append">
-                                <button id="map_shareable_link_copy_to_clipboard" class="btn btn-info"
-                                        data-toggle="tooltip" title="{{ __('Copy shareable link to clipboard') }}">
-                                    <i class="far fa-copy"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="input-group">
-                            {!! Form::text('map_embedable_link',
-                            sprintf('<iframe src="%s" style="width: 800px; height: 600px; border: none;"></iframe>', route('dungeonroute.embed', ['dungeonroute' => $model])),
-                            ['id' => 'map_embedable_link', 'class' => 'form-control', 'readonly' => 'readonly']) !!}
-                            <div class="input-group-append">
-                                <button id="map_embedable_link_copy_to_clipboard" class="btn btn-info"
-                                        data-toggle="tooltip" title="{{ __('Copy embed code to clipboard') }}">
-                                    <i class="far fa-copy"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('common.maps.share', ['model' => $model])
     @endisset
 
     <!-- Tags -->
