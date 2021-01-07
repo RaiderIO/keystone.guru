861be4b76d2093b7f5e16e1f7e271e64bd61c8b9
Wouter Koppenol <wouter.koppenol@gmail.com>
2021-01-07 22:58:24 +0100

    #351 Re-imported dungeons after changes to the database.

--- app/Console/Commands/Mapping/Save.php
+++ app/Console/Commands/Mapping/Save.php
@@ -118,7 +118,7 @@ class Save extends Command
                 $demoRoute->setAppends([]);
                 // Ids cannot be guaranteed with users uploading dungeonroutes as well. As such, a new internal ID must be created
                 // for each and every re-import
-                $demoRoute->setHidden(['id', 'thumbnail_updated_at']);
+                $demoRoute->setHidden(['id', 'thumbnail_updated_at', 'published', 'unlisted']);
                 $demoRoute->load(['playerspecializations', 'playerraces', 'playerclasses',
                                   'routeattributesraw', 'affixgroups', 'brushlines', 'paths', 'killzones', 'enemyraidmarkers',
                                   'pridefulenemies', 'mapicons']);
