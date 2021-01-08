0415bf2c84c6eed87160692d9f791aae6946224e
Wotuu <support@keystone.guru>
2021-01-08 16:45:44 +0100

    #591 Some changes related to importing Shadowlands demo routes

--- resources/views/common/dungeon/demoroutesgrid.blade.php
+++ resources/views/common/dungeon/demoroutesgrid.blade.php
@@ -1,7 +1,7 @@
 <?php
 // @TODO This should be handled differently - defined both here and in app.blade.php
 /** @var \Illuminate\Support\Collection|\App\Models\DungeonRoute[] $demoRoutes */
-$demoRoutes = \App\Models\DungeonRoute::where('demo', true)->where('published_state_id', 4)->orderBy('dungeon_id')->get();
+$demoRoutes = \App\Models\DungeonRoute::where('demo', true)->where('published_state_id', 3)->orderBy('dungeon_id')->get();
 ?>
 
 @include('common.dungeon.grid', [
