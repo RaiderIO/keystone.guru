4be89b5b91fd93a9b4039047ca3e2f78a7619f10
Wotuu <support@keystone.guru>
2021-01-08 16:43:39 +0100

    #596 MDT affix importer corrected

--- app/Logic/MDT/Conversion.php
+++ app/Logic/MDT/Conversion.php
@@ -148,18 +148,18 @@ class Conversion
     {
         // You can do this in a mathy way but tbh I can't be bothered right now.
         $weekMapping = [
-            1  => 12,
-            2  => 1,
-            3  => 2,
-            4  => 3,
-            5  => 4,
-            6  => 5,
-            7  => 6,
-            8  => 7,
-            9  => 8,
-            10 => 9,
-            11 => 10,
-            12 => 11
+            1  => 11,
+            2  => 12,
+            3  => 1,
+            4  => 2,
+            5  => 3,
+            6  => 4,
+            7  => 5,
+            8  => 6,
+            9  => 7,
+            10 => 8,
+            11 => 9,
+            12 => 10
         ];
         return AffixGroup::find($weekMapping[$mdtWeek] + (($seasonService->getSeasons()->count() - 1) * config('keystoneguru.season_iteration_affix_group_count')));
     }
