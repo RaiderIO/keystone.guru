bb8a7c98595d0431c36b15d5b6516af8a6cb4049
Wotuu <wouter.koppenol@gmail.com>
2021-03-10 23:52:58 +0100

    #693 Dungeonroute card opacity issue is fixed.

--- resources/views/common/dungeonroute/card.blade.php
+++ resources/views/common/dungeonroute/card.blade.php
@@ -34,68 +34,71 @@ $enemyForcesWarning = $dungeonroute->enemy_forces < $dungeonroute->dungeon->enem
 
                     </div>
                     <div class="dungeon_background_cover p-2">
-                        @endif
-                        <div class="row no-gutters">
-                            <div class="col">
-                                <h4 class="card-title mb-1">
-                                    <a href="{{ route('dungeonroute.view', ['dungeonroute' => $dungeonroute->public_key]) }}"
-                                       target="_blank">
-                                        {{ $dungeonroute->title }}
-                                    </a>
-                                </h4>
-                            </div>
-                            <div class="col-auto">
-                                @if( $showAffixes )
-                                    <?php
-                                    $isTyrannical = $dungeonroute->isTyrannical();
-                                    $isFortified = $dungeonroute->isFortified();
-                                    ob_start();
-                                    ?>
-                                    @foreach($dungeonroute->affixes as $affixGroup)
-                                        <div class="row no-gutters">
-                                            @include('common.affixgroup.affixgroup', ['affixGroup' => $affixGroup, 'showText' => false])
-                                        </div>
-                                    @endforeach
-                                    <?php $affixes = ob_get_clean(); ?>
-                                    @if($isTyrannical && $isFortified)
-                                        <div data-container="body" data-toggle="popover" data-placement="bottom"
-                                             data-html="true"
-                                             data-content="{{ $affixes }}" style="cursor: pointer;">
-                                            <img class="select_icon" src="{{ url('/images/affixes/keystone.jpg') }}"/>
-                                        </div>
-                                    @elseif($isTyrannical)
-                                        <div data-container="body" data-toggle="popover" data-placement="bottom"
-                                             data-html="true"
-                                             data-content="{{ $affixes }}" style="cursor: pointer;">
-                                            <img class="select_icon" src="{{ url('/images/affixes/tyrannical.jpg') }}"/>
-                                        </div>
-                                    @elseif($isFortified)
-                                        <div data-container="body" data-toggle="popover" data-placement="bottom"
-                                             data-html="true"
-                                             data-content="{{ $affixes }}" style="cursor: pointer;">
-                                            <img class="select_icon" src="{{ url('/images/affixes/fortified.jpg') }}"/>
-                                        </div>
+                            @endif
+                            <div class="row no-gutters">
+                                <div class="col">
+                                    <h4 class="card-title mb-1">
+                                        <a href="{{ route('dungeonroute.view', ['dungeonroute' => $dungeonroute->public_key]) }}"
+                                           target="_blank">
+                                            {{ $dungeonroute->title }}
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="col-auto">
+                                    @if( $showAffixes )
+                                        <?php
+                                        $isTyrannical = $dungeonroute->isTyrannical();
+                                        $isFortified = $dungeonroute->isFortified();
+                                        ob_start();
+                                        ?>
+                                        @foreach($dungeonroute->affixes as $affixGroup)
+                                            <div class="row no-gutters">
+                                                @include('common.affixgroup.affixgroup', ['affixGroup' => $affixGroup, 'showText' => false])
+                                            </div>
+                                        @endforeach
+                                        <?php $affixes = ob_get_clean(); ?>
+                                        @if($isTyrannical && $isFortified)
+                                            <div data-container="body" data-toggle="popover" data-placement="bottom"
+                                                 data-html="true"
+                                                 data-content="{{ $affixes }}" style="cursor: pointer;">
+                                                <img class="select_icon"
+                                                     src="{{ url('/images/affixes/keystone.jpg') }}"/>
+                                            </div>
+                                        @elseif($isTyrannical)
+                                            <div data-container="body" data-toggle="popover" data-placement="bottom"
+                                                 data-html="true"
+                                                 data-content="{{ $affixes }}" style="cursor: pointer;">
+                                                <img class="select_icon"
+                                                     src="{{ url('/images/affixes/tyrannical.jpg') }}"/>
+                                            </div>
+                                        @elseif($isFortified)
+                                            <div data-container="body" data-toggle="popover" data-placement="bottom"
+                                                 data-html="true"
+                                                 data-content="{{ $affixes }}" style="cursor: pointer;">
+                                                <img class="select_icon"
+                                                     src="{{ url('/images/affixes/fortified.jpg') }}"/>
+                                            </div>
+                                        @endif
                                     @endif
-                                @endif
+                                </div>
                             </div>
-                        </div>
-                        <div class="row no-gutters">
-                            <div class="col">
-                                @if( $enemyForcesWarning )
-                                    <span class="text-warning"> <i class="fas fa-exclamation-triangle"></i> </span>
-                                @else
-                                    <span class="text-success"> <i class="fas fa-check-circle"></i> </span>
-                                @endif
-                                {{ sprintf(
-                                    __('%s/%s (%s%%)'),
-                                    $dungeonroute->enemy_forces,
-                                    $dungeonroute->dungeon->enemy_forces_required,
-                                    $enemyForcesPercentage
-                                    ) }}
+                            <div class="row no-gutters">
+                                <div class="col">
+                                    @if( $enemyForcesWarning )
+                                        <span class="text-warning"> <i class="fas fa-exclamation-triangle"></i> </span>
+                                    @else
+                                        <span class="text-success"> <i class="fas fa-check-circle"></i> </span>
+                                    @endif
+                                    {{ sprintf(
+                                        __('%s/%s (%s%%)'),
+                                        $dungeonroute->enemy_forces,
+                                        $dungeonroute->dungeon->enemy_forces_required,
+                                        $enemyForcesPercentage
+                                        ) }}
+                                </div>
                             </div>
-                        </div>
 
-                        @if($showDungeonImage)
+                            @if($showDungeonImage)
                     </div>
                 @endif
             </div>
