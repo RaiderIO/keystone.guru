cef2f011e9abb77a4c43f125a603a4a943becd65
Wotuu <support@keystone.guru>
2021-04-02 14:44:49 +0200

    #717 Affix page given another look - looks better in both themes now.

--- resources/views/misc/affixes.blade.php
+++ resources/views/misc/affixes.blade.php
@@ -5,129 +5,133 @@
 
 $region = \App\Models\GameServerRegion::getUserOrDefaultRegion();
 $timezone = null;
-if (Auth::check()) {
+if (Auth::check())
+{
     $timezone = Auth::user()->timezone;
 }
-if ($timezone === null) {
+if ($timezone === null)
+{
     $timezone = config('app.timezone');
 }
 ?>
-@section('header-title', sprintf(__('Weekly affixes in %s'), $region->name))
-
-@include('common.general.inline', ['path' => 'dungeonroute/discover/discover',
-        'options' =>  [
-        ]
-])
-
+@include('common.general.inline', ['path' => 'dungeonroute/discover/discover'])
 
 @section('content')
     <div class="discover_panel">
-        <table class="affixes_overview_table table-striped" width="100%">
-            <thead>
-            <tr>
-                <th width="20%">
-                    {{ __('Start date') . sprintf(' (%s)', $timezone) }}
-                </th>
-                <th width="20%">
-                    {{ __('+2') }}
-                </th>
-                <th width="20%">
-                    {{ __('+4') }}
-                </th>
-                <th width="20%">
-                    {{ __('+7') }}
-                </th>
-                <th width="20%">
-                    {{ __('+10 (Seasonal)') }}
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title text-center"> {{ sprintf(__('Weekly affixes in %s'), $region->name) }}</h5>
 
-            // Whatever group we're highlighting
-            $currentAffixGroup = $seasonService->getCurrentSeason()->getCurrentAffixGroup();
+                <table class="affixes_overview_table table-striped bg-secondary" width="100%">
+                    <thead>
+                    <tr>
+                        <th width="20%">
+                            {{ __('Start date') . sprintf(' (%s)', $timezone) }}
+                        </th>
+                        <th width="20%">
+                            {{ __('+2') }}
+                        </th>
+                        <th width="20%">
+                            {{ __('+4') }}
+                        </th>
+                        <th width="20%">
+                            {{ __('+7') }}
+                        </th>
+                        <th width="20%">
+                            {{ __('+10 (Seasonal)') }}
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
 
-            $affixGroups = $seasonService->getDisplayedAffixGroups($offset);
-            $affixGroupIndex = 0;
-            foreach($affixGroups as $index => $arr){
-            /** @var \Illuminate\Support\Carbon $startDate */
-            $startDate = $arr['date_start'];
-            /** @var \App\Models\AffixGroup $affixGroup */
-            $affixGroup = $arr['affixgroup'];
-            ?>
-            <tr class="table_row">
-                <?php
-                // Current week if we found the current affix group for this region
-                $currentWeekClass = $affixGroup->id === $currentAffixGroup->id && $startDate->diffInWeeks(\Carbon\Carbon::now()) <= 1 ? 'current_week ' : '';
-                ?>
-                <td>
-                    <div class="affix_row first_column {{ $currentWeekClass }}">
+                    // Whatever group we're highlighting
+                    $currentAffixGroup = $seasonService->getCurrentSeason()->getCurrentAffixGroup();
+
+                    $affixGroups = $seasonService->getDisplayedAffixGroups($offset);
+                    $affixGroupIndex = 0;
+                    foreach($affixGroups as $index => $arr){
+                    /** @var \Illuminate\Support\Carbon $startDate */
+                    $startDate = $arr['date_start'];
+                    /** @var \App\Models\AffixGroup $affixGroup */
+                    $affixGroup = $arr['affixgroup'];
+                    ?>
+                    <tr class="table_row">
+                        <?php
+                        // Current week if we found the current affix group for this region
+                        $currentWeekClass = $affixGroup->id === $currentAffixGroup->id && $startDate->diffInWeeks(\Carbon\Carbon::now()) <= 1 ? 'current_week ' : '';
+                        ?>
+                        <td>
+                            <div class="affix_row first_column {{ $currentWeekClass }}">
                     <span>
                         {{ $startDate->format('Y/M/d') }}
                     </span>
-                        <span class="d-xl-inline d-none">
+                                <span class="d-xl-inline d-none">
                         {{ $startDate->format(' @ H\h') }}
                     </span>
-                    </div>
-                </td>
-                <?php
-                $affixIndex = 0;
-                foreach($affixGroup->affixes as $affix) {
-                $lastColumn = count($affixGroup->affixes) - 1 === $affixIndex;
-                $class = $currentWeekClass;
-                $class .= $lastColumn ? 'last_column ' : '';
-                $class .= ($affixGroupIndex === 0) ? 'first_row ' : '';
-                $class .= $affixGroups->count() - 1 === $affixGroupIndex ? 'last_row ' : '';
-                ?>
-                <td>
-                    <div class="affix_row {{ $class }}">
-                        <div class="row no-gutters">
-                            <div class="col-auto select_icon class_icon affix_icon_{{ strtolower($affix->name) }}"
-                                 data-toggle="tooltip"
-                                 title="{{ $affix->description }}"
-                                 style="height: 24px;">
                             </div>
-                            <div class="col d-lg-block d-none pl-1">
-                                {{ $affix->name }}
-                                @if($lastColumn)
-                                    @if($affixGroup->season->presets > 0 )
-                                        {{ sprintf(__('preset %s'), $affixGroup->season->getPresetAt($startDate)) }}
-                                    @endif
-                                    @isset($affixGroup->seasonal_index)
-                                        {{ sprintf(__('(%s)'), $affixGroup->getSeasonalIndexAsLetter()) }}
-                                    @endisset
-                                @endif
+                        </td>
+                        <?php
+                        $affixIndex = 0;
+                        foreach($affixGroup->affixes as $affix) {
+                        $lastColumn = count($affixGroup->affixes) - 1 === $affixIndex;
+                        $class = $currentWeekClass;
+                        $class .= $lastColumn ? 'last_column ' : '';
+                        $class .= ($affixGroupIndex === 0) ? 'first_row ' : '';
+                        $class .= $affixGroups->count() - 1 === $affixGroupIndex ? 'last_row ' : '';
+                        ?>
+                        <td>
+                            <div class="affix_row {{ $class }}">
+                                <div class="row no-gutters">
+                                    <div
+                                        class="col-auto select_icon class_icon affix_icon_{{ strtolower($affix->name) }}"
+                                        data-toggle="tooltip"
+                                        title="{{ $affix->description }}"
+                                        style="height: 24px;">
+                                    </div>
+                                    <div class="col d-lg-block d-none pl-1">
+                                        {{ $affix->name }}
+                                        @if($lastColumn)
+                                            @if($affixGroup->season->presets > 0 )
+                                                {{ sprintf(__('preset %s'), $affixGroup->season->getPresetAt($startDate)) }}
+                                            @endif
+                                            @isset($affixGroup->seasonal_index)
+                                                {{ sprintf(__('(%s)'), $affixGroup->getSeasonalIndexAsLetter()) }}
+                                            @endisset
+                                        @endif
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        </td><?php
+                        $affixIndex++;
+                        }
+                        $affixGroupIndex++
+                        ?>
+                    </tr><?php
+                    } ?>
+                    </tbody>
+                </table>
+
+                <div class="row mt-2">
+                    <div class="col">
+
                     </div>
-                </td><?php
-                $affixIndex++;
-                }
-                $affixGroupIndex++
-                ?>
-            </tr><?php
-            } ?>
-            </tbody>
-        </table>
+                    <div class="col-auto">
+                        <ul class="pagination" role="navigation">
+                            <li class="page-item">
+                                <a class="page-link" href="{{ route('misc.affixes', ['offset' => $offset - 1]) }}">
+                                    ‹ {{ __('Previous') }}
+                                </a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="{{ route('misc.affixes', ['offset' => $offset + 1]) }}">
+                                    {{ __('Next') }} ›
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
 
-        <div class="row mt-2">
-            <div class="col">
-
-            </div>
-            <div class="col-auto">
-                <ul class="pagination" role="navigation">
-                    <li class="page-item">
-                        <a class="page-link" href="{{ route('misc.affixes', ['offset' => $offset - 1]) }}">
-                            ‹ {{ __('Previous') }}
-                        </a>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link" href="{{ route('misc.affixes', ['offset' => $offset + 1]) }}">
-                            {{ __('Next') }} ›
-                        </a>
-                    </li>
-                </ul>
             </div>
         </div>
 
