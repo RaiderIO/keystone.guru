fb66deafc57fb1e093639aa65def7b22077795ca
Wotuu <support@keystone.guru>
2021-04-12 21:35:01 +0200

    #733 WIP adding my favorites page

--- resources/views/profile/routes.blade.php
+++ resources/views/profile/routes.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.sitepage', ['rootClass' => 'col-xl-10 offset-xl-1', 'wide' => true, 'title' => __('My routes')])
+@extends('layouts.sitepage', ['rootClass' => 'col-xl-10 offset-xl-1', 'wide' => true, 'title' => __('My favorited routes')])
 
 @section('content')
     @include('common.general.messages')
 
-    @include('common.dungeonroute.table', ['view' => 'profile'])
+    @include('common.dungeonroute.table', ['view' => 'favorites'])
 @endsection
