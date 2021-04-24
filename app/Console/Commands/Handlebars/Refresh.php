a3498525d046fc54f8bccb3353383e2b2732f95a
Wotuu <wouter.koppenol@gmail.com>
2021-04-24 23:16:41 +0200

    #776 Added enemy forces to tooltips of enemies in the pulls sidebar

---
+++ app/Console/Commands/Handlebars/Refresh.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands\Handlebars;
+
+use App\Console\Commands\Traits\ExecutesShellCommands;
+use Illuminate\Console\Command;
+
+class Refresh extends Command
+{
+    use ExecutesShellCommands;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'handlebars:refresh';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Refreshes the handlebars compiled file';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->shell([
+            sprintf(
+                'handlebars %s resources/assets/js/handlebars/ -f resources/assets/js/handlebars.js',
+                env('APP_ENV') === 'production' ? '-m' : ''
+            )
+        ]);
+
+        $this->info('Handlebars refreshed');
+
+        return 0;
+    }
+}
