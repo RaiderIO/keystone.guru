c672494f6430d16f51bc555dfc38792aa3a8cbd7
Wotuu <wouter.koppenol@gmail.com>
2021-04-25 15:42:49 +0200

    #768 Added Laravel Telescope

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
 
         /**
          * Custom
