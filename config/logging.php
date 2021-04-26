ab25c816f5daa6ff0f101e21fe17604de6f09463
Wotuu <wouter.koppenol@gmail.com>
2021-04-26 23:37:33 +0200

    #783 Updated logging to support Discord (again). Exceptions logged will now be more visible.

--- config/logging.php
+++ config/logging.php
@@ -3,18 +3,6 @@
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
 
-$discord = [];
-
-// If it's set only!
-if (!empty(env('APP_LOG_DISCORD_WEBHOOK'))) {
-    $discord['discord'] = [];/*
-        'driver' => 'custom',
-        'url' => 'https://discordapp.com/api/webhooks/' . env('APP_LOG_DISCORD_WEBHOOK'),
-        'via' => App\Logging\CreateDiscordLogger::class,
-        'level' => 'error',
-    ];*/
-}
-
 return [
     /*
     |--------------------------------------------------------------------------
@@ -26,7 +14,7 @@ return [
     | one of the channels defined in the "channels" configuration array.
     |
     */
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default'  => env('LOG_CHANNEL', 'stack'),
     /*
     |--------------------------------------------------------------------------
     | Log Channels
@@ -41,66 +29,68 @@ return [
     |                    "custom", "stack"
     |
     */
-    'channels' => array_merge($discord, [
-        'stack' => [
-            'driver' => 'stack',
-            'channels' => ['daily']//, 'discord'],
+    'channels' => [
+        'stack'          => [
+            'driver'   => 'stack',
+            'channels' => ['daily', 'discord'],
         ],
-        'scheduler' => [
-            'driver' => 'stack',
-            'channels' => ['scheduler_file']//, 'discord'],
+        'scheduler'      => [
+            'driver'   => 'stack',
+            'channels' => ['scheduler_file'],
         ],
-        'single' => [
+        'single'         => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
+            'path'   => storage_path('logs/laravel.log'),
+            'level'  => 'debug',
         ],
         'scheduler_file' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/scheduler.log'),
-            'level' => 'debug',
-            'days' => 14,
+            'path'   => storage_path('logs/scheduler.log'),
+            'level'  => 'debug',
+            'days'   => 14,
         ],
-        'daily' => [
+        'daily'          => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
-            'days' => 14,
+            'path'   => storage_path('logs/laravel.log'),
+            'level'  => 'debug',
+            'days'   => 14,
         ],
-        'slack' => [
-            'driver' => 'slack',
-            'url' => env('LOG_SLACK_WEBHOOK_URL'),
+        'slack'          => [
+            'driver'   => 'slack',
+            'url'      => env('LOG_SLACK_WEBHOOK_URL'),
             'username' => 'Laravel Log',
-            'emoji' => ':boom:',
-            'level' => 'critical',
+            'emoji'    => ':boom:',
+            'level'    => 'critical',
         ],
-        'papertrail' => [
-            'driver' => 'monolog',
-            'level' => 'debug',
-            'handler' => SyslogUdpHandler::class,
+        'papertrail'     => [
+            'driver'       => 'monolog',
+            'level'        => 'debug',
+            'handler'      => SyslogUdpHandler::class,
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
             ],
         ],
-        'stderr' => [
-            'driver' => 'monolog',
+        'stderr'         => [
+            'driver'  => 'monolog',
             'handler' => StreamHandler::class,
-            'with' => [
+            'with'    => [
                 'stream' => 'php://stderr',
             ],
         ],
-        'syslog' => [
+        'syslog'         => [
             'driver' => 'syslog',
-            'level' => 'debug',
+            'level'  => 'debug',
         ],
-        'errorlog' => [
+        'errorlog'       => [
             'driver' => 'errorlog',
-            'level' => 'debug',
+            'level'  => 'debug',
         ],
-//        'discord' => [
-//            'driver' => 'stack',
-//            'channels' => ['daily'],
-//        ],
-    ]),
+        'discord'        => empty(env('APP_LOG_DISCORD_WEBHOOK')) ? [] : [
+            'driver' => 'custom',
+            'url'    => env('APP_LOG_DISCORD_WEBHOOK'),
+            'via'    => MarvinLabs\DiscordLogger\Logger::class,
+            'level'  => 'debug',
+        ]
+    ],
 ];
