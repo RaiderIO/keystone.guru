d0034c14f5296746594b496de406fd7f4230a4ab
Wotuu <wouter.koppenol@gmail.com>
2021-11-03 23:55:46 +0100

    #902 Updated various bits and pieces on the embed page to make it cleaner & better

--- resources/views/dungeonroute/embed.blade.php
+++ resources/views/dungeonroute/embed.blade.php
@@ -40,30 +40,43 @@ if (count($affixes) == 0) {
 @section('content')
     <header class="header_embed"
             style="background-image: url('/images/dungeons/{{$dungeon->expansion->shortname}}/{{$dungeon->key}}.jpg'); background-size: cover;">
-        <div class="row no-gutters">
-            <div class="col-8">
-                <a href="{{ route('dungeonroute.view', ['dungeonroute' => $dungeonroute]) }}" target="_blank">
-                    <h4>
-                        {{ $dungeonroute->title }}
-                    </h4>
-                </a>
+        <div class="row no-gutters py-2">
+            <div class="col-8 pt-2">
+                <div class="row no-gutters">
+                    <div class="col">
+                        <a class="text-white" href="{{ route('dungeonroute.view', ['dungeonroute' => $dungeonroute]) }}"
+                           target="_blank">
+                            <h4 class="mb-0">
+                                {{ $dungeonroute->title }}
+                            </h4>
+                        </a>
+                    </div>
+                </div>
+                <div class="row no-gutters">
+                    <div class="col">
+                        {{ __('views/dungeonroute.embed.create_or_view_at') }}
+                        <a class="text-white" href="{{ route('dungeonroute.view', ['dungeonroute' => $dungeonroute]) }}"
+                           target="_blank">
+                            {{ route('dungeonroute.view', ['dungeonroute' => $dungeonroute]) }}
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="col-4">
+            <div class="col-4 text-right">
                 <a href="{{ route('home') }}" target="_blank">
-                    <h4 class="text-right">
-                        {{ config('app.name') }}
-                    </h4>
+                    <img src="{{ url('/images/logo/logo_and_text.png') }}" alt="{{ config('app.name') }}"
+                         height="44px;" width="200px;">
                 </a>
             </div>
         </div>
         <div class="row no-gutters">
-            <div class="col-4">
+            <div class="col-3">
                 <div class="embed-header-subtitle">
                     <?php // This is normally in the pulls sidebar - but for embedding it's in the header - see pulls.blade.php ?>
                     <div id="edit_route_enemy_forces_container"></div>
                 </div>
             </div>
-            <div class="col-4">
+            <div class="col-3">
                 <?php // Select floor thing is a place holder because otherwise the selectpicker will complain on an empty select ?>
                 @if($dungeon->floors()->count() > 1)
                     {!! Form::select('map_floor_selection', [__('views/dungeonroute.embed.select_floor')], 1, ['id' => 'map_floor_selection', 'class' => 'form-control selectpicker']) !!}
@@ -79,13 +92,13 @@ if (count($affixes) == 0) {
                     'data-selected-text-format' => 'count > 1',
                     'data-count-selected-text' => __('views/dungeonroute.embed.affixes_selected')]) !!}
             </div>
-            <div class="col-1">
-                <div id="embed_copy_mdt_string" class="btn btn-primary float-right" data-toggle="tooltip"
-                     title="{{ __('views/dungeonroute.embed.copy_mdt_string') }}">
-                    <i class="fas fa-file-export"></i>
+            <div class="col-3">
+                <div id="embed_copy_mdt_string" class="btn btn-primary float-right">
+                    <i class="fas fa-file-export"></i> {{ __('views/dungeonroute.embed.copy_mdt_string') }}
                 </div>
-                <div id="embed_copy_mdt_string_loader" class="btn btn-primary float-right" style="display: none;">
-                    <i class="fas fa-circle-notch fa-spin"></i>
+                <div id="embed_copy_mdt_string_loader" class="btn btn-primary float-right" disabled
+                     style="display: none;">
+                    <i class="fas fa-circle-notch fa-spin"></i> {{ __('views/dungeonroute.embed.copy_mdt_string') }}
                 </div>
             </div>
         </div>
