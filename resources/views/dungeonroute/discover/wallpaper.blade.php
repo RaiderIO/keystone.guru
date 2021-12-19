ad3dbb22cb288e8a62909d9fdff1104dc56d85f0
Wotuu <wouter.koppenol@gmail.com>
2021-12-19 15:54:58 +0100

    #931 Fixed affixgroup display in search page

--- resources/views/dungeonroute/discover/wallpaper.blade.php
+++ resources/views/dungeonroute/discover/wallpaper.blade.php
@@ -1,13 +1,15 @@
+<?php
+// @formatter:off
+// ^ important - it BREAKS the display of wallpapers when the contents of url('') get a new line..
+?>
 <div class="dungeon_wallpaper"
      @if($dungeon->expansion->shortname === \App\Models\Expansion::EXPANSION_LEGION)
-     style="background-image: url('{{ url(sprintf('/images/dungeons/%s/wallpaper.jpg', $dungeon->expansion->shortname)) }}')">
-    @else
+        style="background-image: url('{{ url(sprintf('/images/dungeons/%s/wallpaper.jpg', $dungeon->expansion->shortname)) }}')">
+     @else
         style="background-image:
-        url('{{ url(sprintf('/images/dungeons/%s/%s_wallpaper.jpg', $dungeon->expansion->shortname, $dungeon->key)) }}
-        ')">
+        url('{{ url(sprintf('/images/dungeons/%s/%s_wallpaper.jpg', $dungeon->expansion->shortname, $dungeon->key)) }}')">
     @endif
 </div>
 <div class="dungeon_wallpaper_cover">
 </div>
-{{--<div class="dungeon_wallpaper_gradient">--}}
-{{--</div>--}}
+<?php // @formatter:on ?>
