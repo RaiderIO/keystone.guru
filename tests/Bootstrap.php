0df7cf4c7767a6416f53228b82e6e927fa716e27
Wotuu <wouter.koppenol@gmail.com>
2022-01-11 00:05:29 +0100

    #945 Added new cache file to excluded files.

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -12,11 +12,11 @@ trait Bootstrap
      */
     private function bootstrap(): void
     {
-        Artisan::call('migrate', ['--database' => 'phpunit', '--force' => true]);
-        // Only seed if we need to
-        if (Season::count() === 0) {
-            Artisan::call('db:seed', ['--database' => 'phpunit']);
-        }
+//        Artisan::call('migrate', ['--database' => 'phpunit', '--force' => true]);
+//        // Only seed if we need to
+//        if (Season::count() === 0) {
+//            Artisan::call('db:seed', ['--database' => 'phpunit']);
+//        }
     }
 
 }
