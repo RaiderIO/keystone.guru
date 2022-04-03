42f781117e88e8c299c87809912e1245928093b8
Wotuu <wouter.koppenol@gmail.com>
2022-04-03 15:09:29 +0200

    #1042 Fixed fonts not loading properly - echo not connecting

--- webpack.mix.js
+++ webpack.mix.js
@@ -75,6 +75,7 @@ if (typeof argv.env !== 'undefined' && typeof argv.env.images !== 'undefined') {
 }
 
 mix.copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts');
+mix.copy('resources/assets/webfonts', 'public/webfonts');
 
 let precompile = [
     // Translations
