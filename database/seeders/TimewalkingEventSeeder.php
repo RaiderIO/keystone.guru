a94eddfbce85ac63964291a52f66169ce6086f7f
Wotuu <wouter.koppenol@gmail.com>
2022-04-05 17:48:57 +0200

    #1044 Corrected Legion timewalking schedule v2

--- database/seeders/TimewalkingEventSeeder.php
+++ database/seeders/TimewalkingEventSeeder.php
@@ -28,8 +28,8 @@ class TimewalkingEventSeeder extends Seeder
                 'expansion_id'         => Expansion::where('shortname', Expansion::EXPANSION_LEGION)->first()->id,
                 'name'                 => 'timewalkingevent.legion.name',
                 'key'                  => TimewalkingEvent::TIMEWALKING_EVENT_LEGION,
-                'start'                => '2021-12-28 00:00:00',
-                'start_duration_weeks' => 1,
+                'start'                => '2021-12-07 00:00:00',
+                'start_duration_weeks' => 4,
                 'week_interval'        => 18,
             ],
             //            [
