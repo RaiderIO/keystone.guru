05230ae8c378ea8b842142a9c9f470cc1624bec2
Wotuu <wouter.koppenol@gmail.com>
2022-04-05 13:35:01 +0200

    #1046 Thumbnails are now queued again if they're in the queue for 12 hours (which means that it's not going to happen anymore)

--- app/Logic/Scheduler/RefreshOutdatedThumbnails.php
+++ app/Logic/Scheduler/RefreshOutdatedThumbnails.php
@@ -23,13 +23,21 @@ class RefreshOutdatedThumbnails
 
         /** @var DungeonRoute[]|Collection $routes */
         $routes = DungeonRoute::where('author_id', '>', '0')
-            ->whereColumn('thumbnail_refresh_queued_at', '<', 'thumbnail_updated_at')
+            // Check if in queue, if so skip, unless the queue age is longer than keystoneguru.thumbnail.refresh_requeue_hours
+            ->where(function (Builder $builder) {
+                $builder->whereColumn('thumbnail_refresh_queued_at', '<', 'thumbnail_updated_at')
+                    ->orWhere(function (Builder $builder) {
+                        // If it is in the queue to be refreshed
+                        $builder->whereColumn('thumbnail_refresh_queued_at', '>', 'thumbnail_updated_at')
+                            ->whereDate('thumbnail_refresh_queued_at', '<', now()->subHours(config('keystoneguru.thumbnail.refresh_requeue_hours')));
+                    });
+            })
             ->where(function (Builder $builder) {
                 // Only if it's not already queued!
                 $builder->where(function (Builder $builder) {
                     $builder->whereColumn('updated_at', '>', 'thumbnail_updated_at')
                         ->whereDate('updated_at', '<', now()->subMinutes(config('keystoneguru.thumbnail.refresh_min'))->toDateTimeString());
-                })->orWhere('thumbnail_updated_at', '<', now()->subDays(config('keystoneguru.thumbnail.refresh_anyways_days')));
+                })->orWhereDate('thumbnail_updated_at', '<', now()->subDays(config('keystoneguru.thumbnail.refresh_anyways_days')));
             })
             // Published routes get priority! This is only really relevant initially while processing the thumbnail queue
             ->orderBy('published_state_id', 'desc')
--- config/keystoneguru.php
+++ config/keystoneguru.php
@@ -171,6 +171,11 @@ return [
          * The amount of days where the thumbnail gets refreshed anyways regardless of other rules.
          */
         'refresh_anyways_days' => 30,
+
+        /**
+         * The amount of hours where a thumbnail refresh must be in the queue for before it is re-queued
+         */
+        'refresh_requeue_hours' => 12,
     ],
 
     /**
