c7b551d77153c157da34df6ad1bd5da5857566d2
Wotuu <wouter.koppenol@gmail.com>
2022-08-08 10:12:04 +0200

    #1290 Fixed MDT auto solver not taking MDT_NPC_ID override into account

--- resources/assets/js/custom/inline/common/maps/map.js
+++ resources/assets/js/custom/inline/common/maps/map.js
@@ -725,7 +725,9 @@ class CommonMapsMap extends InlineCode {
         for (let index in enemyMapObjectGroup.objects) {
             let enemyCandidate = enemyMapObjectGroup.objects[index];
             let distance = getDistance([enemyCandidate.lat, enemyCandidate.lng], [targetEnemy.lat, targetEnemy.lng]);
-            if (enemyCandidate.is_mdt && targetEnemy.npc_id === enemyCandidate.npc_id && targetEnemy.floor_id === enemyCandidate.floor_id && closestEnemyDistance > distance) {
+            if (enemyCandidate.is_mdt &&
+                targetEnemy.getMdtNpcId() === enemyCandidate.npc_id &&
+                targetEnemy.floor_id === enemyCandidate.floor_id && closestEnemyDistance > distance) {
                 result = enemyCandidate;
                 closestEnemyDistance = distance;
             }
