b8e4a9472157708913d36a8ac1f248f766b0b722
Wotuu <wouter.koppenol@gmail.com>
2022-11-22 23:28:46 +0100

    #1462 Reverted typing in caching service - was causing crashes

--- app/Service/Cache/CacheServiceInterface.php
+++ app/Service/Cache/CacheServiceInterface.php
@@ -7,13 +7,13 @@ interface CacheServiceInterface
 {
     public function setCacheEnabled(bool $cacheEnabled): self;
 
-    public function rememberWhen(bool $condition, string $key, $value, ?string $ttl = null);
+    public function rememberWhen(bool $condition, string $key, $value, $ttl = null);
 
-    public function remember(string $key, $value, ?string $ttl = null);
+    public function remember(string $key, $value, $ttl = null);
 
     public function get(string $key);
 
-    public function set(string $key, $object, ?string $ttl = null): bool;
+    public function set(string $key, $object, $ttl = null): bool;
 
     public function has(string $key): bool;
 
