3422a227f8a402a335228b25a992287451e1b306
Wotuu <wouter.koppenol@gmail.com>
2022-12-16 10:11:03 +0100

    #1506 Added structured logging to PatreonApiService

--- app/Service/Patreon/PatreonApiService.php
+++ app/Service/Patreon/PatreonApiService.php
@@ -2,36 +2,59 @@
 
 namespace App\Service\Patreon;
 
-use App\Logic\SimulationCraft\RaidEventsCollection;
-use App\Models\SimulationCraft\SimulationCraftRaidEventsOptions;
+use App\Service\Patreon\Logging\PatreonApiServiceLoggingInterface;
 use Patreon\API;
 use Patreon\OAuth;
 
 class PatreonApiService implements PatreonApiServiceInterface
 {
+    private PatreonApiServiceLoggingInterface $log;
+
+    /**
+     * @param PatreonApiServiceLoggingInterface $log
+     */
+    public function __construct(PatreonApiServiceLoggingInterface $log)
+    {
+        $this->log = $log;
+    }
+
+
     /**
      * @param string $accessToken
-     * @return array|null
+     * @return array{errors: array|null, included: array|null}|null
      */
     public function getIdentity(string $accessToken): ?array
     {
-        $identityResponse = $this->getApiClient($accessToken)->get_data(
-            sprintf('identity?include=memberships,memberships.currently_entitled_tiers' .
-                '&%s=email,first_name,full_name,image_url,last_name,thumb_url,url,vanity,is_email_verified' .
-                '&%s=email,currently_entitled_amount_cents,lifetime_support_cents,last_charge_status,patron_status,last_charge_date,pledge_relationship_start',
-                urlencode('fields[user]'),
-                urlencode('fields[member]')
-            )
-        );
+        $this->log->getIdentityStart();
+        $identityResponse = null;
 
-        if (!isset($identityResponse['errors'])) {
-            // Bit ugly but otherwise I'd need the broad 'campaigns.members[email]' permission which I don't need/want
-            foreach ($identityResponse['included'] as &$included) {
-                if ($included['type'] === 'member') {
-                    $included['attributes']['email'] = $identityResponse['data']['attributes']['email'];
-                    break;
+        try {
+            $identityResponse = $this->getApiClient($accessToken)->get_data(
+                sprintf('identity?include=memberships,memberships.currently_entitled_tiers' .
+                    '&%s=email,first_name,full_name,image_url,last_name,thumb_url,url,vanity,is_email_verified' .
+                    '&%s=email,currently_entitled_amount_cents,lifetime_support_cents,last_charge_status,patron_status,last_charge_date,pledge_relationship_start',
+                    urlencode('fields[user]'),
+                    urlencode('fields[member]')
+                )
+            );
+
+            if (!isset($identityResponse['errors'])) {
+
+                if (!isset($identityResponse['included'])) {
+                    $this->log->getIdentityIncludedNotFound();
+                } else {
+                    // Bit ugly but otherwise I'd need the broad 'campaigns.members[email]' permission which I don't need/want
+                    foreach ($identityResponse['included'] as &$included) {
+                        if ($included['type'] === 'member') {
+                            $included['attributes']['email'] = $identityResponse['data']['attributes']['email'];
+                            $this->log->getIdentityUpdatedEmailAddress($included['attributes']['email']);
+                            break;
+                        }
+                    }
                 }
             }
+        } finally {
+            $this->log->getIdentityEnd($identityResponse);
         }
 
         return $identityResponse;
@@ -44,13 +67,23 @@ class PatreonApiService implements PatreonApiServiceInterface
      */
     public function getCampaignTiersAndBenefits(string $accessToken): ?array
     {
-        return $this->getAllData(
-            $this->getApiClient($accessToken),
-            sprintf('campaigns/%d?include=tiers,tiers.benefits&%s=title',
-                config('keystoneguru.patreon.campaign_id'),
-                urlencode('fields[benefit]')
-            )
-        );
+        $result = null;
+
+        try {
+            $this->log->getCampaignTiersAndBenefitsStart();
+
+            $result = $this->getAllPages(
+                $this->getApiClient($accessToken),
+                sprintf('campaigns/%d?include=tiers,tiers.benefits&%s=title',
+                    config('keystoneguru.patreon.campaign_id'),
+                    urlencode('fields[benefit]')
+                )
+            );
+        } finally {
+            $this->log->getCampaignTiersAndBenefitsEnd($result);
+        }
+
+        return $result;
     }
 
 
@@ -60,13 +93,22 @@ class PatreonApiService implements PatreonApiServiceInterface
      */
     public function getCampaignMembers(string $accessToken): ?array
     {
-        return $this->getAllData(
-            $this->getApiClient($accessToken),
-            sprintf('campaigns/%d/members?include=currently_entitled_tiers&%s=email',
-                config('keystoneguru.patreon.campaign_id'),
-                urlencode('fields[member]')
-            )
-        );
+        $result = null;
+
+        try {
+            $this->log->getCampaignMembersStart();
+            $result = $this->getAllPages(
+                $this->getApiClient($accessToken),
+                sprintf('campaigns/%d/members?include=currently_entitled_tiers&%s=email',
+                    config('keystoneguru.patreon.campaign_id'),
+                    urlencode('fields[member]')
+                )
+            );
+        } finally {
+            $this->log->getCampaignMembersEnd($result);
+        }
+
+        return $result;
     }
 
 
@@ -94,12 +136,14 @@ class PatreonApiService implements PatreonApiServiceInterface
      * @param string $suffix
      * @return array
      */
-    private function getAllData(API $apiClient, string $suffix): array
+    private function getAllPages(API $apiClient, string $suffix): array
     {
         $resultData = [];
 
-        $next = $suffix;
+        $next  = $suffix;
+        $count = 0;
         do {
+            $this->log->getAllPagesPageNr($count);
             $requestResult = $apiClient->get_data($next);
             // Insane workaround if you get a 4xx error it won't do json_decode
             if (is_string($requestResult)) {
@@ -117,7 +161,9 @@ class PatreonApiService implements PatreonApiServiceInterface
             } else {
                 // Found an error - just stop it now
                 $next = null;
+                $this->log->getAllPagesError($requestResult['errors']);
             }
+            $count++;
         } while ($next !== null);
 
         // Assign the data back to the last request and pretend that THAT's all the data there is
