c75cc8dadcc88c7e9ed1407d10840e96e89ade0b
Wotuu <wouter.koppenol@gmail.com>
2022-12-25 09:55:53 +0100

    #1499 Added logging bindings

--- app/Providers/LoggingServiceProvider.php
+++ app/Providers/LoggingServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Service\Patreon\Logging\PatreonApiServiceLogging;
 use App\Service\Patreon\Logging\PatreonApiServiceLoggingInterface;
+use App\Service\Patreon\Logging\PatreonServiceLogging;
+use App\Service\Patreon\Logging\PatreonServiceLoggingInterface;
 use App\Service\WowTools\Logging\WowToolsServiceLogging;
 use App\Service\WowTools\Logging\WowToolsServiceLoggingInterface;
 use Illuminate\Support\ServiceProvider;
@@ -17,6 +19,7 @@ class LoggingServiceProvider extends ServiceProvider
     {
         parent::register();
 
+        $this->app->bind(PatreonServiceLoggingInterface::class, PatreonServiceLogging::class);
         $this->app->bind(PatreonApiServiceLoggingInterface::class, PatreonApiServiceLogging::class);
         $this->app->bind(WowToolsServiceLoggingInterface::class, WowToolsServiceLogging::class);
     }
