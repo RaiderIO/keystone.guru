1ec4d7046b6f2026e39f646887148d5e6acaf141
Wouter Koppenol <wouter.koppenol@gmail.com>
2023-08-09 23:00:25 +0200

    #1936 Added implementation of the logging

--- app/Service/CombatLog/Logging/CombatLogDataExtractionServiceLogging.php
+++ app/Service/CombatLog/Logging/CombatLogDataExtractionServiceLogging.php
@@ -3,9 +3,67 @@
 namespace App\Service\CombatLog\Logging;
 
 use App\Logging\StructuredLogging;
-use App\Service\CombatLog\Logging\CombatLogDataExtractionServiceLoggingInterface;
 
 class CombatLogDataExtractionServiceLogging extends StructuredLogging implements CombatLogDataExtractionServiceLoggingInterface
 {
+    /**
+     * @return void
+     */
+    public function extractDataTimestampNotSet(): void
+    {
+        $this->warning(__METHOD__);
+    }
 
+    /**
+     * @param string $dungeonName
+     * @param int    $keyLevel
+     * @param string $affixGroup
+     *
+     * @return void
+     */
+    public function extractDataSetChallengeMode(string $dungeonName, int $keyLevel, string $affixGroup): void
+    {
+        $this->debug(__METHOD__, get_defined_vars());
+    }
+
+    /**
+     * @param string $dungeonName
+     *
+     * @return void
+     */
+    public function extractDataSetZone(string $dungeonName): void
+    {
+        $this->debug(__METHOD__, get_defined_vars());
+    }
+
+    /**
+     * @param int $previousFloorId
+     * @param int $currentFloorId
+     *
+     * @return void
+     */
+    public function extractDataAddedNewFloorConnection(int $previousFloorId, int $currentFloorId)
+    {
+        $this->debug(__METHOD__, get_defined_vars());
+    }
+
+    /**
+     * @param int $npcId
+     *
+     * @return void
+     */
+    public function extractDataNpcNotFound(int $npcId): void
+    {
+        $this->notice(__METHOD__, get_defined_vars());
+    }
+
+    /**
+     * @param int $baseHealth
+     *
+     * @return void
+     */
+    public function extractDataUpdatedNpc(int $baseHealth): void
+    {
+        $this->debug(__METHOD__, get_defined_vars());
+    }
 }
