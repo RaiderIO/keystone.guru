ee3e41c54ee29cf82d4d9a4b6c58ef1ed18a70c7
Wouter Koppenol <wouter.koppenol@gmail.com>
2023-10-29 23:12:59 +0100

    #2047 Scaling and rotating works completely now.

--- tests/Unit/App/Logic/Structs/LatLngTest.php
+++ tests/Unit/App/Logic/Structs/LatLngTest.php
@@ -28,7 +28,6 @@ class LatLngTest extends PublicTestCase
         $targetCenter   = new LatLng(100, 100);
         $targetMapSize  = 200;
 
-
         // Act
         $result = $latLng->scale($currentCenter, $currentMapSize, $targetCenter, $targetMapSize);
 
@@ -93,10 +92,104 @@ class LatLngTest extends PublicTestCase
                 new LatLng(-195, 210),
                 new LatLng(-145.0368, 110.9632),
             ],
-//            [
-//                new LatLng(75, 75),
-//                new LatLng(150, 150),
-//            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @group LatLng
+     *
+     * @param LatLng $latLng
+     * @param int    $rotation
+     * @param LatLng $expected
+     * @return void
+     * @dataProvider rotate_givenPositiveMapCoordinates_shouldRotateLatLng_DataProvider
+     */
+    public function rotate_givenPositiveMapCoordinates_shouldRotateLatLng(
+        LatLng $latLng,
+        int    $rotation,
+        LatLng $expected
+    ): void {
+        // Arrange
+        $currentCenter = new LatLng(50, 50);
+
+        // Act
+        $result = $latLng->rotate($currentCenter, $rotation);
+
+        // Assert
+        Assert::assertEquals($expected->getLat(), $result->getLat());
+        Assert::assertEquals($expected->getLng(), $result->getLng());
+    }
+
+    /**
+     * @return array
+     */
+    public function rotate_givenPositiveMapCoordinates_shouldRotateLatLng_DataProvider(): array
+    {
+        return [
+            // Top left to top right
+            [
+                new LatLng(25, 25),
+                90,
+                new LatLng(25, 75),
+            ],
+            // Top left to dead middle
+            [
+                new LatLng(25, 25),
+                45,
+                new LatLng(14.644660940672622, 50),
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @group LatLng2
+     *
+     * @param LatLng $latLng
+     * @param int    $rotation
+     * @param LatLng $expected
+     * @return void
+     * @dataProvider rotate_givenScaledMapCoordinates_shouldRotateLatLng_DataProvider
+     */
+    public function rotate_givenScaledMapCoordinates_shouldRotateLatLng(
+        LatLng $latLng,
+        int    $rotation,
+        LatLng $expected
+    ): void {
+        // Arrange
+        $currentCenter  = new LatLng(50, 50);
+        $currentMapSize = 100;
+        $targetCenter   = new LatLng(100, 100);
+        $targetMapSize  = 200;
+
+        // Act
+        $result = $latLng->scale($currentCenter, $currentMapSize, $targetCenter, $targetMapSize)
+            ->rotate($targetCenter, $rotation);
+
+        // Assert
+        Assert::assertEquals($expected->getLat(), $result->getLat());
+        Assert::assertEquals($expected->getLng(), $result->getLng());
+    }
+
+    /**
+     * @return array
+     */
+    public function rotate_givenScaledMapCoordinates_shouldRotateLatLng_DataProvider(): array
+    {
+        return [
+            // Top left to top right
+            [
+                new LatLng(25, 25),
+                90,
+                new LatLng(50, 150),
+            ],
+            // Top left to dead middle
+            //            [
+            //                new LatLng(25, 25),
+            //                45,
+            //                new LatLng(14.644660940672622, 50),
+            //            ],
         ];
     }
 }
