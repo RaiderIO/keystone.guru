5dd79b756be2eb29d2ee506a4d090c5b47a53679
Wouter Koppenol <wouter.koppenol@gmail.com>
2023-11-02 15:54:15 +0100

    #2035 Re-added Throne of the Tides dungeon (through MDT this time)

---
+++ database/migrations/2023_11_02_140451_change_vertices_json_column_to_text_in_floor_union_areas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeVerticesJsonColumnToTextInFloorUnionAreasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('floor_union_areas', function (Blueprint $table) {
+            $table->text('vertices_json')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('floor_union_areas', function (Blueprint $table) {
+            $table->string('vertices_json')->change();
+        });
+    }
+}
