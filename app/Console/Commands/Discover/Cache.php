7d4b1707b4c26f68f1c3f9e24a625561330c8d9f
Wotuu <wouter.koppenol@gmail.com>
2023-11-16 11:19:54 +0100

    #2124 Fixed loading a relationship that doesn't exist anymore

--- app/Console/Commands/Discover/Cache.php
+++ app/Console/Commands/Discover/Cache.php
@@ -50,7 +50,7 @@ class Cache extends Command
         $discoverService = $discoverService->withCache(false);
 
         // Refresh caches for all categories
-        foreach (Expansion::with(['dungeons', 'currentSeason'])->active()->get() as $expansion) {
+        foreach (Expansion::with(['dungeons'])->active()->get() as $expansion) {
             /** @var Expansion $expansion */
             $this->info(sprintf('- %s', $expansion->shortname));
 
