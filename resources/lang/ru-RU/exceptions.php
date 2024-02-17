5a8ec09fdd566c2da6d2e2ee0c2645bbf49809ac
Wouter Koppenol <wouter.koppenol@gmail.com>
2024-02-17 13:09:15 +0100

    #2175 Added missing translation files.

---
+++ resources/lang/ru-RU/exceptions.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return [
+    'handler' => [
+        'api_model_not_found'   => '@todo ru-RU: .handler.api_model_not_found',
+        'api_route_not_found'   => '@todo ru-RU: .handler.api_route_not_found',
+        'internal_server_error' => '@todo ru-RU: .handler.internal_server_error',
+        'unauthenticated'       => '@todo ru-RU: .handler.unauthenticated',
+    ],
+];
