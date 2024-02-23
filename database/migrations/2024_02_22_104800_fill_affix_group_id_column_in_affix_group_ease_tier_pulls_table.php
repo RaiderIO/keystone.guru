f5ffc4a9b4b866cee699b848b42db2dfd1f3253a
Wouter Koppenol <wouter.koppenol@gmail.com>
2024-02-23 14:34:40 +0100

    #2210 Fixed migration

--- database/migrations/2024_02_22_104800_fill_affix_group_id_column_in_affix_group_ease_tier_pulls_table.php
+++ database/migrations/2024_02_22_104800_fill_affix_group_id_column_in_affix_group_ease_tier_pulls_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\AffixGroup\AffixGroupEaseTierPull;
 use App\Service\AffixGroup\AffixGroupEaseTierServiceInterface;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Database\Migrations\Migration;
@@ -23,12 +24,19 @@ class FillAffixGroupIdColumnInAffixGroupEaseTierPullsTable extends Migration
 
         foreach ($rows as $row) {
             $row = (array)$row;
-            DB::update('
+
+            $affixGroup = $affixGroupEaseTierService->getAffixGroupByString($row['current_affixes']);
+            if ($affixGroup === null) {
+                // Delete it through Eloquent so that it cleans up the relationships as well
+                AffixGroupEaseTierPull::findOrFail($row['id'])->delete();
+            } else {
+                DB::update('
                 UPDATE affix_group_ease_tier_pulls SET affix_group_id = :affixGroupId WHERE id = :id
             ', [
-                'affixGroupId' => $affixGroupEaseTierService->getAffixGroupByString($row['current_affixes'])->id,
-                'id'           => $row['id'],
-            ]);
+                    'affixGroupId' => $affixGroup->id,
+                    'id'           => $row['id'],
+                ]);
+            }
         }
     }
 
