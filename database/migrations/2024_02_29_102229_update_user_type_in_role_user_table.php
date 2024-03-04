48ec9443f6efbfd9d1a3f3f6e5068a9ab10b0db8
Wotuu <wouter.koppenol@gmail.com>
2024-03-04 15:44:03 +0100

    #2214 Finally really fixed the Laratrust migration.
    Just needed MORE BACKSLASHES

--- database/migrations/2024_02_29_102229_update_user_type_in_role_user_table.php
+++ database/migrations/2024_02_29_102229_update_user_type_in_role_user_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         DB::update('
-            UPDATE `role_user` SET `user_type` = "App\\Models\\User" WHERE `user_type` = "App\\User"
+            UPDATE `role_user` SET `user_type` = "App\\\\Models\\\\User" WHERE `user_type` = "App\\\\User"
         ');
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         DB::update('
-            UPDATE `role_user` SET `user_type` = "App\\User" WHERE `user_type` = "App\\Models\\User"
+            UPDATE `role_user` SET `user_type` = "App\\\\User" WHERE `user_type` = "App\\\\Models\\\\User"
         ');
     }
 };
