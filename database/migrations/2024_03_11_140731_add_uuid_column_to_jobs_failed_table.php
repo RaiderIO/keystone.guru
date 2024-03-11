e7c4396f09cbaeb27f7bd49106d044a2b2a60f2f
Wotuu <wouter.koppenol@gmail.com>
2024-03-11 15:10:41 +0100

    #2267 Fixed unknown column UUID error.

---
+++ database/migrations/2024_03_11_140731_add_uuid_column_to_jobs_failed_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('jobs_failed', function (Blueprint $table) {
+            $table->string('uuid')->after('id')->nullable()->unique();
+        });
+
+        DB::table('jobs_failed')->whereNull('uuid')->cursor()->each(function ($job) {
+            DB::table('jobs_failed')
+                ->where('id', $job->id)
+                ->update(['uuid' => (string) Illuminate\Support\Str::uuid()]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('jobs_failed', function (Blueprint $table) {
+            $table->dropColumn('uuid');
+        });
+    }
+};
