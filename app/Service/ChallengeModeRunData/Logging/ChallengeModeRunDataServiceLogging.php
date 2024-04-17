ba23db1e4ebdd1f5d7dd3a43e6d5296713e2f30e
Wotuu <wouter.koppenol@gmail.com>
2024-04-17 17:09:16 +0200

    #2308 Added missing logging implementation.
    Also changed the Opensearch ports as to not cause conflicts with other running containers

--- app/Service/ChallengeModeRunData/Logging/ChallengeModeRunDataServiceLogging.php
+++ app/Service/ChallengeModeRunData/Logging/ChallengeModeRunDataServiceLogging.php
@@ -6,5 +6,34 @@ use App\Logging\StructuredLogging;
 
 class ChallengeModeRunDataServiceLogging extends StructuredLogging implements ChallengeModeRunDataServiceLoggingInterface
 {
+    public function convertStart(): void
+    {
+        $this->start(__METHOD__);
+    }
+
+    public function convertEnd(): void
+    {
+        $this->end(__METHOD__);
+    }
+
+    public function convertChallengeModeRunDataStart(): void
+    {
+        $this->start(__METHOD__);
+    }
+
+    public function convertChallengeModeRunDataUnableToFindDungeon(int $mapId): void
+    {
+        $this->warning(__METHOD__, get_defined_vars());
+    }
+
+    public function convertChallengeModeRunDefaultAttributes(array $defaultAttributes): void
+    {
+        $this->debug(__METHOD__, get_defined_vars());
+    }
+
+    public function convertChallengeModeRunDataEnd(int $count): void
+    {
+        $this->end(__METHOD__, get_defined_vars());
+    }
 
 }
--- app/Service/ChallengeModeRunData/Logging/ChallengeModeRunDataServiceLoggingInterface.php
+++ app/Service/ChallengeModeRunData/Logging/ChallengeModeRunDataServiceLoggingInterface.php
@@ -4,5 +4,15 @@ namespace App\Service\ChallengeModeRunData\Logging;
 
 interface ChallengeModeRunDataServiceLoggingInterface
 {
+    public function convertStart(): void;
 
+    public function convertEnd(): void;
+
+    public function convertChallengeModeRunDataStart(): void;
+
+    public function convertChallengeModeRunDataUnableToFindDungeon(int $mapId): void;
+
+    public function convertChallengeModeRunDefaultAttributes(array $defaultAttributes): void;
+
+    public function convertChallengeModeRunDataEnd(int $count): void;
 }
