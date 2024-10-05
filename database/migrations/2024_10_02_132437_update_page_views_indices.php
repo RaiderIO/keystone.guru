8d916c089816906cc07147a961a2b5308388c7e0
Wouter Koppenol <wouter.koppenol@gmail.com>
2024-10-05 13:42:51 +0200

    #2534 Made sure that the query can run, time out, then run again and pass. Just in case deployment times out because of the creation of this index.

--- database/migrations/2024_10_02_132437_update_page_views_indices.php
+++ database/migrations/2024_10_02_132437_update_page_views_indices.php
@@ -4,17 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('page_views', function (Blueprint $table) {
-            $table->dropIndex('page_views_model_class_index');
+        set_time_limit(999999);
 
-            $table->index(['model_class', 'created_at', 'model_id'], 'page_views_popularity_index');
+        Schema::table('page_views', function (Blueprint $table) {
+            if (Schema::hasIndex('page_views', 'page_views_popularity_index')) {
+                $table->dropIndex('page_views_model_class_index');
+            }
+
+            if (!Schema::hasIndex('page_views', 'page_views_popularity_index')) {
+                $table->index(['model_class', 'created_at', 'model_id'], 'page_views_popularity_index');
+            }
         });
     }
 
@@ -23,10 +28,16 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('page_views', function (Blueprint $table) {
-            $table->dropIndex('page_views_popularity_index');
+        set_time_limit(999999);
 
-            $table->index(['model_class']);
+        Schema::table('page_views', function (Blueprint $table) {
+            if (Schema::hasIndex('page_views', 'page_views_popularity_index')) {
+                $table->dropIndex('page_views_popularity_index');
+            }
+
+            if (!Schema::hasIndex('page_views', 'page_views_model_class_index')) {
+                $table->index('page_views_model_class_index');
+            }
         });
     }
 };
