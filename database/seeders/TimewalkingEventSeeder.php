b7fa06adeb8bc4450f3eedd0ffc0501aca609b38
Wotuu <wouter.koppenol@gmail.com>
2024-10-09 10:34:52 +0200

    #2514 Legion timewalking is removed.

--- database/seeders/TimewalkingEventSeeder.php
+++ database/seeders/TimewalkingEventSeeder.php
@@ -17,14 +17,14 @@ class TimewalkingEventSeeder extends Seeder implements TableSeederInterface
     public function run(): void
     {
         $timewalkingEventsAttributes = [
-            TimewalkingEvent::TIMEWALKING_EVENT_LEGION => [
-                'expansion_id'         => Expansion::where('shortname', Expansion::EXPANSION_LEGION)->first()->id,
-                'name'                 => 'timewalkingevent.legion.name',
-                'key'                  => TimewalkingEvent::TIMEWALKING_EVENT_LEGION,
-                'start'                => '2021-12-07 00:00:00',
-                'start_duration_weeks' => 4,
-                'week_interval'        => 18,
-            ],
+            //            TimewalkingEvent::TIMEWALKING_EVENT_LEGION => [
+            //                'expansion_id'         => Expansion::where('shortname', Expansion::EXPANSION_LEGION)->first()->id,
+            //                'name'                 => 'timewalkingevent.legion.name',
+            //                'key'                  => TimewalkingEvent::TIMEWALKING_EVENT_LEGION,
+            //                'start'                => '2021-12-07 00:00:00',
+            //                'start_duration_weeks' => 4,
+            //                'week_interval'        => 18,
+            //            ],
             //            [
             //                'expansion_id'         => Expansion::where('shortname', Expansion::EXPANSION_BFA)->first()->id,
             //                'name'                 => 'timewalkingevent.bfa.name',
