841159803cefe1b77bcd7a895a463c7dd0b49f54
Wotuu <wouter.koppenol@gmail.com>
2024-10-11 14:25:34 +0200

    #2559 Fixed redis channels no longer going to DB 0 by default for non-production environments, but use the configured DB for that environment instead.

--- laravel-echo-server.js
+++ laravel-echo-server.js
@@ -16,8 +16,9 @@ EchoServer.run({
     "database": "redis",
     "databaseConfig": {
         "redis": {
-            "port": 6379,
-            "host": "127.0.0.1"
+            "host": env.REDIS_HOST,
+            "port": env.REDIS_PORT,
+            "db": env.REDIS_DB
         },
         "publishPresence": true
     },
