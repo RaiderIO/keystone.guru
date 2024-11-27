78e8d5645fd584f9b40367e50a1c5cadf99dd02e
Wouter Koppenol <wouter.koppenol@gmail.com>
2024-11-27 22:21:31 +0100

    #2527 Searching for dungeons on the NPC table now searches the translated string instead of the translation key.

--- app/Logic/Datatables/ColumnHandler/Npc/DungeonColumnHandler.php
+++ app/Logic/Datatables/ColumnHandler/Npc/DungeonColumnHandler.php
@@ -10,11 +10,25 @@ namespace App\Logic\Datatables\ColumnHandler\Npc;
 
 use App\Logic\Datatables\ColumnHandler\SimpleColumnHandler;
 use App\Logic\Datatables\DatatablesHandler;
+use DB;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\JoinClause;
 
 class DungeonColumnHandler extends SimpleColumnHandler
 {
     public function __construct(DatatablesHandler $dtHandler)
     {
         parent::__construct($dtHandler, 'dungeon_id', 'dungeons.name');
+    }
+
+    protected function applyFilter(Builder $subBuilder, $columnData, $order, $generalSearch): void
+    {
+        // Only order
+        $this->getDtHandler()->getBuilder()->leftJoin('translations', static function (JoinClause $clause) {
+            $clause->on('translations.key', 'dungeons.name')
+                ->on('translations.locale', DB::raw('"en_US"'));
+        });
+
+        $subBuilder->orWhere('translations.translation', 'LIKE', sprintf('%%%s%%', $generalSearch));
     }
 }
