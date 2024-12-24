e5f8bf0782a512d0d50297542ced7fbecb23f42e
Wotuu <wouter.koppenol@gmail.com>
2024-12-24 11:46:43 +0100

    #2463 Added support for v22 ENVIRONMENTAL_DAMAGE

--- app/Logic/CombatLog/SpecialEvents/EnvironmentalDamage/EnvironmentalDamageBuilder.php
+++ app/Logic/CombatLog/SpecialEvents/EnvironmentalDamage/EnvironmentalDamageBuilder.php
@@ -4,6 +4,7 @@ namespace App\Logic\CombatLog\SpecialEvents\EnvironmentalDamage;
 
 use App\Logic\CombatLog\CombatLogVersion;
 use App\Logic\CombatLog\SpecialEvents\EnvironmentalDamage\Versions\V20\EnvironmentalDamageV20;
+use App\Logic\CombatLog\SpecialEvents\EnvironmentalDamage\Versions\V22\EnvironmentalDamageV22;
 use App\Logic\CombatLog\SpecialEvents\EnvironmentalDamage\Versions\V9\EnvironmentalDamageV9;
 use App\Logic\CombatLog\SpecialEvents\SpecialEvent;
 use App\Logic\CombatLog\SpecialEvents\SpecialEventBuilderInterface;
@@ -20,7 +21,8 @@ class EnvironmentalDamageBuilder implements SpecialEventBuilderInterface
     ): SpecialEvent {
         return match ($combatLogVersion) {
             CombatLogVersion::CLASSIC => new EnvironmentalDamageV9($combatLogVersion, $timestamp, $eventName, $parameters, $rawEvent),
-            default => new EnvironmentalDamageV20($combatLogVersion, $timestamp, $eventName, $parameters, $rawEvent),
+            CombatLogVersion::RETAIL_10_1_0, CombatLogVersion::RETAIL_11_0_2 => new EnvironmentalDamageV20($combatLogVersion, $timestamp, $eventName, $parameters, $rawEvent),
+            default => new EnvironmentalDamageV22($combatLogVersion, $timestamp, $eventName, $parameters, $rawEvent),
         };
     }
 }
---
+++ app/Logic/CombatLog/SpecialEvents/EnvironmentalDamage/Versions/V22/EnvironmentalDamageV22.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Logic\CombatLog\SpecialEvents\EnvironmentalDamage\Versions\V22;
+
+use App\Logic\CombatLog\CombatEvents\Interfaces\HasParameters;
+use App\Logic\CombatLog\SpecialEvents\SpecialEvent;
+
+/**
+ * 11:45:23.5980  ENVIRONMENTAL_DAMAGE,0000000000000000,nil,0x80000000,0x80000000,Player-1335-09E309B2,\"Éclairochoco-Ysondre-EU\",0x512,0x0,Player-1335-09E309B2,0000000000000000,6585299,6612606,71503,17650,36151,66,300,0,0,2500000,2500000,0,-460.68,-370.82,293,5.7054,630,Falling,27307,27307,0,1,0,0,0,nil,nil,nil
+ *
+ * @author Wouter
+ *
+ * @since 23/12/2024
+ */
+class EnvironmentalDamageV22 extends SpecialEvent
+{
+    /**
+     * @return HasParameters|$this
+     */
+    public function setParameters(array $parameters): HasParameters
+    {
+        parent::setParameters($parameters);
+
+        return $this;
+    }
+
+    public function getParameterCount(): int
+    {
+        return 38;
+    }
+}
