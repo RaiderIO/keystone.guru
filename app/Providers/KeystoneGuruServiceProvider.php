ea67885378e38564c002ce13bf84c231c7650255
Wouter Koppenol <wouter.koppenol@gmail.com>
2025-01-24 23:19:56 +0100

    #2678 Unit tests now pass. I think.

--- app/Providers/KeystoneGuruServiceProvider.php
+++ app/Providers/KeystoneGuruServiceProvider.php
@@ -94,6 +94,7 @@ use App\Service\Patreon\PatreonService;
 use App\Service\Patreon\PatreonServiceInterface;
 use App\Service\RaiderIO\RaiderIOApiService;
 use App\Service\RaiderIO\RaiderIOApiServiceInterface;
+use App\Service\RaiderIO\RaiderIOKeystoneGuruApiService;
 use App\Service\ReadOnlyMode\ReadOnlyModeService;
 use App\Service\ReadOnlyMode\ReadOnlyModeServiceInterface;
 use App\Service\Reddit\RedditApiService;
@@ -207,8 +208,12 @@ class KeystoneGuruServiceProvider extends ServiceProvider
         $this->app->bind(WowToolsServiceInterface::class, WowToolsService::class);
         $this->app->bind(AdProviderServiceInterface::class, AdProviderService::class);
         $this->app->bind(WowheadServiceInterface::class, WowheadService::class);
-        $this->app->bind(RaiderIOApiServiceInterface::class, RaiderIOApiService::class);
-//        $this->app->bind(RaiderIOApiServiceInterface::class, RaiderIOKeystoneGuruApiService::class);
+        if (app()->runningUnitTests()) {
+            $this->app->bind(RaiderIOApiServiceInterface::class, RaiderIOKeystoneGuruApiService::class);
+        } else {
+            $this->app->bind(RaiderIOApiServiceInterface::class, RaiderIOApiService::class);
+        }
+//
         $this->app->bind(CloudflareServiceInterface::class, CloudflareService::class);
 
         // Depends on CombatLogService, SeasonService, WowheadService
