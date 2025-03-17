bb0e28e51b6f90ab713aeb47c9e61b8d2d663f05
Wouter Koppenol <wouter.koppenol@gmail.com>
2025-03-17 12:06:59 +0100

    #2771 Added Priory of the Sacred Flame event correction tests

--- tests/Feature/Controller/Api/V1/APICombatLogController/CorrectEvents/TWW/APICombatLogControllerCorrectEventsPrioryOfTheSacredFlameTest.php
+++ tests/Feature/Controller/Api/V1/APICombatLogController/CorrectEvents/TWW/APICombatLogControllerCorrectEventsPrioryOfTheSacredFlameTest.php
@@ -11,17 +11,17 @@ use Tests\Feature\Controller\Api\V1\APICombatLogController\CorrectEvents\APIComb
 #[Group('API')]
 #[Group('APICombatLog')]
 #[Group('CorrectEvents')]
-#[Group('DarkflameCleft')]
-class APICombatLogControllerCorrectEventsDarkflameCleftTest extends APICombatLogControllerCorrectEventsTestBase
+#[Group('PrioryOfTheSacredFlame')]
+class APICombatLogControllerCorrectEventsPrioryOfTheSacredFlameTest extends APICombatLogControllerCorrectEventsTestBase
 {
     protected function getDungeonKey(): string
     {
-        return Dungeon::DUNGEON_DARKFLAME_CLEFT;
+        return Dungeon::DUNGEON_PRIORY_OF_THE_SACRED_FLAME;
     }
 
     #[Test]
-    public function create_givenDarkflameCleft5Json_shouldReturnCorrectedJsonData(): void
+    public function create_givenPrioryOfTheSacredFlame14Json_shouldReturnCorrectedJsonData(): void
     {
-        $this->executeTest('TWW/tww_s2_darkflame_cleft_5');
+        $this->executeTest('TWW/tww_s2_priory_of_the_sacred_flame_14');
     }
 }
