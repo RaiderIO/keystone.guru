3988c2a62fbbdf316a60e044735ed78715d999c9
Wouter Koppenol <wouter.koppenol@gmail.com>
2025-07-15 13:14:46 +0200

    #2880 Fixed switching game modes when you're not logged in not allowing you to navigate the explore overview page properly

--- app/Models/GameVersion/GameVersion.php
+++ app/Models/GameVersion/GameVersion.php
@@ -96,6 +96,6 @@ class GameVersion extends CacheModel
         /** @var CacheServiceInterface $cacheService */
         $cacheService = App::make(CacheServiceInterface::class);
 
-        return $cacheService->remember('default_game_version', static fn() => GameVersion::where('key', self::DEFAULT_GAME_VERSION)->first());
+        return $cacheService->remember('default_game_version', static fn() => GameVersion::firstWhere('key', self::DEFAULT_GAME_VERSION));
     }
 }
