9e05afba211a0759236f664f671221b7386d91f7
Wouter Koppenol <wouter.koppenol@gmail.com>
2025-07-20 21:39:21 +0200

    #2896 Fixed two issues when testing on staging

--- app/Service/Cookies/CookieService.php
+++ app/Service/Cookies/CookieService.php
@@ -7,13 +7,17 @@ class CookieService implements CookieServiceInterface
     public function setCookie(string $key, mixed $value, int $expires = 0, string $path = '/', string $domain = null, bool $secure = true, bool $httponly = false): void
     {
         $_COOKIE[$key] = $value;
-        cookie()->queue(cookie($key, $value, [
-            'expires'  => $expires,
-            'path'     => $path,
-            'domain'   => $domain,
-            'secure'   => config('app.env') === 'local' ? false : $secure,
-            'httponly' => $httponly,
-        ]));
+        cookie()->queue(
+            cookie(
+                $key,
+                $value,
+                $expires,
+                $path,
+                $domain,
+                config('app.env') === 'local' ? false : $secure,
+                $httponly
+            )
+        );
     }
 
 }
