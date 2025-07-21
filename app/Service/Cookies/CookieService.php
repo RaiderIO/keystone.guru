fc43f36c0a57a1df988e17c41187f13c3b6eccef
Wouter Koppenol <wouter.koppenol@gmail.com>
2025-07-21 22:34:19 +0200

    #2896 Fixed an issue with the cookie service, added wallpapers for all MoP dungeons.

--- app/Service/Cookies/CookieService.php
+++ app/Service/Cookies/CookieService.php
@@ -7,16 +7,14 @@ class CookieService implements CookieServiceInterface
     public function setCookie(string $key, mixed $value, int $expires = 0, string $path = '/', string $domain = null, bool $secure = true, bool $httponly = false): void
     {
         $_COOKIE[$key] = $value;
-        cookie()->queue(
-            cookie(
-                $key,
-                $value,
-                $expires,
-                $path,
-                $domain,
-                config('app.env') === 'local' ? false : $secure,
-                $httponly
-            )
+        setcookie(
+            $key,
+            $value,
+            $expires,
+            $path,
+            $domain,
+            config('app.env') === 'local' ? false : $secure,
+            $httponly
         );
     }
 
