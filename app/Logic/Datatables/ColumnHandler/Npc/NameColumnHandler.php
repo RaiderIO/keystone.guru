e20fb0993719fc3fe00636e2736a18c0a06165b0
Wouter Koppenol <wouter.koppenol@gmail.com>
2025-10-03 10:28:50 +0200

    #2982 Filtering on NPC name now works again

--- app/Logic/Datatables/ColumnHandler/Npc/NameColumnHandler.php
+++ app/Logic/Datatables/ColumnHandler/Npc/NameColumnHandler.php
@@ -10,11 +10,28 @@ namespace App\Logic\Datatables\ColumnHandler\Npc;
 
 use App\Logic\Datatables\ColumnHandler\SimpleColumnHandler;
 use App\Logic\Datatables\DatatablesHandler;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\JoinClause;
 
 class NameColumnHandler extends SimpleColumnHandler
 {
     public function __construct(DatatablesHandler $dtHandler)
     {
-        parent::__construct($dtHandler, 'name', 'npcs.name');
+        parent::__construct($dtHandler, 'name', 'npc_name_translations.translation');
+    }
+
+    protected function applyFilter(
+        Builder $subBuilder,
+        Builder $orderBuilder,
+                $columnData,
+                $order,
+                $generalSearch,
+    ): void {
+        // Only order
+        $subBuilder
+            ->leftJoin('translations as npc_name_translations', function (JoinClause $clause) {
+                $clause->on('npc_name_translations.key', '=', 'npcs.name')
+                    ->where('npc_name_translations.locale', '=', 'en_US');
+            })->orWhere('npc_name_translations.translation', 'LIKE', sprintf('%%%s%%', $generalSearch));
     }
 }
