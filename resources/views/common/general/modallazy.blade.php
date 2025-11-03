ef82fb58b3683fc4660cd4b883c206cd2a29f06f
Wouter Koppenol <wouter.koppenol@gmail.com>
2025-11-03 21:32:23 +0100

    #3016 Reduced duplicated code

--- resources/views/common/general/modallazy.blade.php
+++ resources/views/common/general/modallazy.blade.php
@@ -2,43 +2,25 @@
 /**
  * @var string $targetView
  */
-
 $class     ??= '';
 $size      ??= 'md';
 $static    ??= false;
 $active    ??= false;
 $showClose ??= true;
 ?>
-@if( $active )
-    @include('common.general.inline', ['path' => 'modal/active', 'options' => [
-        'id' => '#' . $id,
-    ]])
-@endif
 @include('common.general.inline', ['path' => 'modal/lazy', 'options' => [
     'id' => '#' . $id,
     'view' => $targetView,
 ]])
-
-<div class="modal fade" id="{{ $id }}" tabindex="-1" role="dialog" aria-hidden="true" data-keyboard="false"
-     @if($static)
-         data-backdrop="static"
-    @endif>
-    <div class="{{ $class }} modal-dialog modal-{{$size}} vertical-align-center">
-        <div class="modal-content">
-            @if($showClose)
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-                    <i class="fas fa-times"></i>
-                </button>
-            @endif
-            <div class="probootstrap-modal-flex">
-                <div class="probootstrap-modal-content">
-
-                    <div class="text-center my-5">
-                        <i class="fas fa-spinner fa-pulse fa-3x"></i>
-                    </div>
-
-                </div>
-            </div>
-        </div>
+@component('common.general.modal', [
+    'id' => $id,
+    'class' => $class,
+    'size' => $size,
+    'static' => $static,
+    'active' => $active,
+    'showClose' => $showClose,
+])
+    <div class="text-center my-5">
+        <i class="fas fa-spinner fa-pulse fa-3x"></i>
     </div>
-</div>
+@endcomponent
