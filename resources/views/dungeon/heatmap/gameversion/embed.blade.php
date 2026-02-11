1f17e8f0a1d52d6d7935a554febde8d232caa5c2
Wouter Koppenol <wouter.koppenol@gmail.com>
2026-02-11 14:42:57 +0100

    #3079 Fixed embed size showing a bar at the bottom when header is hidden

--- resources/views/dungeon/heatmap/gameversion/embed.blade.php
+++ resources/views/dungeon/heatmap/gameversion/embed.blade.php
@@ -25,6 +25,8 @@ use Illuminate\Support\Collection;
  * @var array                        $parameters
  * @var float                        $defaultZoom
  */
+
+$showHeader = !isset($embedOptions['show']['header']) || $embedOptions['show']['header'];
 ?>
 @extends('layouts.map', [
     'showAds' => false,
@@ -48,7 +50,7 @@ use Illuminate\Support\Collection;
 ]])
 
 @section('content')
-    @if(!isset($embedOptions['show']['header']) || $embedOptions['show']['header'])
+    @if($showHeader)
         @include(sprintf('dungeon.heatmap.gameversion.embedheaderstyle.%s', $embedOptions['style']), [
             'gameVersion' => $gameVersion,
             'dungeon' => $dungeon,
@@ -58,7 +60,7 @@ use Illuminate\Support\Collection;
         ])
     @endif
 
-    <div class="wrapper embed_wrapper {{ $embedOptions['style'] }}">
+    <div class="wrapper embed_wrapper {{ $showHeader ? $embedOptions['style'] : '' }}" style="border: 2px solid red;">
         @include('common.maps.map', [
             'dungeon' => $dungeon,
             'mappingVersion' => $dungeon->getCurrentMappingVersion($gameVersion),
