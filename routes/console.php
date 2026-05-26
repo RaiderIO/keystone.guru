241d64f263af78264fc07275b798135d5763b554
Wouter Koppenol <wouter.koppenol@gmail.com>
2026-05-26 15:09:49 +0200

    #3218 Disabled automatic combatlog:pollruns while a required API endpoint is still missing

--- routes/console.php
+++ routes/console.php
@@ -19,11 +19,11 @@ $appType = config('app.type');
 $commands = [];
 
 $commands[] = Schedule::command('combatlog:detectstaledata')->hourly();
-if (in_array($appType, [
-    'production',
-])) {
-    $commands[] = Schedule::command('combatlog:pollruns')->hourly();
-}
+//if (in_array($appType, [
+//    'production',
+//])) {
+//    $commands[] = Schedule::command('combatlog:pollruns')->hourly();
+//}
 
 $commands[] = Schedule::command('dungeonroute:updatepopularity')->hourly();
 $commands[] = Schedule::command('dungeonroute:updaterating')->everyFifteenMinutes();
