3d88ab72b539f5a36de3f8f12d90cf34370fde15
Wouter Koppenol <wouter.koppenol@gmail.com>
2026-06-19 20:14:34 +0200

    #3302 Re-done how lazy loading violations are handled

--- tests/Feature/Mapping/DefaultFloorTest.php
+++ tests/Feature/Mapping/DefaultFloorTest.php
@@ -19,7 +19,7 @@ class DefaultFloorTest extends TestCase
         // Arrange
         $zoomLevels = 5;
         /** @var Collection<Dungeon> $dungeons */
-        $dungeons = Dungeon::all();
+        $dungeons = Dungeon::with('floors')->get();
 
         // Act & Assert
         foreach ($dungeons as $dungeon) {
